27f371802037dfc1022452e7ee2804c573b123e5
cmscms419 <alstjr123qwe@gmail.com>
2021-11-22 21:18:11 +0900

    doc.c Update

--- PI01/ras_client.c
+++ PI01/ras_client.c
@@ -99,3 +99,44 @@ void error_handling(char* message)
 	fputc('\n', stderr);
 	exit(1);
 }
+
+// 만약, doc.c가 작동하지 않으면 이 주석을 풀면 됩니다.
+/*
+//  LED Setting
+void led_setting()
+{
+	pinMode(LED_RED, OUTPUT);
+	pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_BLUE, OUTPUT);
+
+	digitalWrite(LED_RED, 0);
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+}
+
+// PIR Setting
+void pir_setting()
+{
+	pinMode(PIR_D, INPUT);
+	wiringPiISR(PIR_D, INT_EDGE_RISING, &PIR_interrupt);
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OnLED()
+{
+	digitalWrite(LED_GREEN, 1);
+	digitalWrite(LED_BLUE, 0);
+
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 1);
+
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OffLED()
+{
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+	digitalWrite(LED_RED, 1);
+}
+*/
