c126d5188dd22b662f4933d122aa1afc6717ea4b
cmscms419 <alstjr123qwe@gmail.com>
2021-11-28 17:02:29 +0900

    raspberry
    라즈베리파이로 만들 서비스 구상하기

---
+++ PI01/doc_server.h
@@ -0,0 +1,79 @@
+﻿#include <wiringPi.h>
+#define CRT_SECURE_NO_WARNINGS
+
+/*
+#define 규칙
+	하드웨어 번호를 부분은 앞에서 부터 번호를 적는다.
+	다른 것은 뒤에서 부터 적는다.
+	
+	 12 34 56
+	 12 : 하드웨어 번호이다.
+	 34 : Wiring Pi 핀 번호이다.
+	 56 : 신호에 대한 정보이다.
+		01 : on
+		00 : off
+		11 : on_off exchange
+
+*/
+
+// cmscms419가 정한 규칙
+#define RED_ON 40701
+#define RED_OFF 40700
+#define GREEN_ON 52101
+#define GREEN_OFF 52100
+#define BLUE_ON 62201
+#define BLUE_OFF 62200
+
+#define PIR_ON 270201
+#define PIR_OFF 270200
+
+//정규 규칙
+#define PIR_D 2
+
+#define LED_RED 7
+#define LED_GREEN 21
+#define LED_BLUE 22
+
+//특수한 상황에서 쓰는 규칙
+
+//EXCHANGE의 규칙
+//2개의 RED wiring Pi 번호를 적는다. 뒤에는 신호에 대한 정보를 적는다.
+#define GB_EXCHANGE 5611
+
+//  LED Setting
+void led_setting()
+{
+	pinMode(LED_RED, OUTPUT);
+	pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_BLUE, OUTPUT);
+
+	digitalWrite(LED_RED, 0);
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+}
+
+// PIR Setting
+void pir_setting()
+{
+	pinMode(PIR_D, INPUT);
+	wiringPiISR(PIR_D, INT_EDGE_RISING, &PIR_interrupt);
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OnLED()
+{
+	digitalWrite(LED_GREEN, 1);
+	digitalWrite(LED_BLUE, 0);
+
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 1);
+
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OffLED()
+{
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+	digitalWrite(LED_RED, 1);
+}
---
+++ raspberry/ras_client.c
@@ -0,0 +1,142 @@
+﻿#include "doc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 100
+
+void error_handling(char* message);
+
+char pir_flag = 0;
+
+void PIR_interrupt()
+{
+	pir_flag = 1;
+}
+
+int main(int argc, char* argv[])
+{
+	int sock;
+	char message[BUF_SIZE];
+	int led = 0;
+	int str_len;
+	struct sockaddr_in serv_adr;
+
+	if (wiringPiSetup() == -1)
+		return 1;
+
+	//IOT setting
+	led_setting();
+	pir_setting();
+
+	if (argc != 3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+		error_handling("socket() error");
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("connect() error!");
+	else
+		puts("Connected...........");
+
+	while (1)
+	{
+		// 서버에게 PID 값을 전달한다.
+		if (pir_flag == 1) 
+		{
+			printf("PIR Detected !! \n");
+			pir_flag = 0;
+
+			strcpy(message, "PIR ON");
+			write(sock, message, BUF_SIZE);
+			led = LED_RED;
+		}
+		else {
+			printf("PIR Not detect !! \n");
+			                                                                                       
+			strcpy(message, "PIR OFF");
+			write(sock, message, BUF_SIZE);
+			led = GB_EXCHANGE;
+		}
+
+		// usleep(50000); // 0.05
+
+		switch (led)
+		{
+		case GB_EXCHANGE:
+			pir_OnLED();
+			break;
+
+		case RED_ON:
+			pir_OffLED();
+			break;
+
+		default:
+			close(sock);
+			return 0;
+		}
+	}
+
+	close(sock);
+	return 0;
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+// 만약, doc.c가 작동하지 않으면 이 주석을 풀면 됩니다.
+/*
+//  LED Setting
+void led_setting()
+{
+	pinMode(LED_RED, OUTPUT);
+	pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_BLUE, OUTPUT);
+
+	digitalWrite(LED_RED, 0);
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+}
+
+// PIR Setting
+void pir_setting()
+{
+	pinMode(PIR_D, INPUT);
+	wiringPiISR(PIR_D, INT_EDGE_RISING, &PIR_interrupt);
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OnLED()
+{
+	digitalWrite(LED_GREEN, 1);
+	digitalWrite(LED_BLUE, 0);
+
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 1);
+
+}
+
+// PIR 작동할 떄 사용하는 모듈
+void pir_OffLED()
+{
+	digitalWrite(LED_GREEN, 0);
+	digitalWrite(LED_BLUE, 0);
+	digitalWrite(LED_RED, 1);
+}
+*/
